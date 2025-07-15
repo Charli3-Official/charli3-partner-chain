07c55d157f1e025c18a6215887ae1c75f5469ab5
stanislav-az <st.zhdanovich@gmail.com>
2025-07-15 16:53:00 +0500

    add oracle message impl

--- toolkit/pallets/oracle/src/lib.rs
+++ toolkit/pallets/oracle/src/lib.rs
@@ -3,9 +3,17 @@
 pub use pallet::*;
 
 use frame_support::pallet_prelude::{BoundedVec, ConstU32};
-use frame_system::pallet_prelude::BlockNumberFor;
+use frame_system::{
+	offchain::{SignMessage, Signer},
+	pallet_prelude::BlockNumberFor,
+};
+use hex::ToHex;
+use pallet_timestamp::{self as timestamp};
+use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use scale_info::prelude::{vec, vec::Vec};
 use sp_core::crypto::KeyTypeId;
+use sp_runtime::SaturatedConversion;
+use sp_std::boxed::Box;
 
 pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"orac");
 
@@ -15,7 +23,7 @@ use price_providers::{CryptoCompareProvider, PriceProvider};
 pub const SCALING_FACTOR: f64 = 10000.0;
 
 pub mod crypto {
-	use crate::KEY_TYPE;
+	use super::KEY_TYPE;
 	use sp_core::ed25519::Signature as Ed25519Signature;
 	use sp_runtime::{
 		app_crypto::{app_crypto, ed25519},
@@ -49,8 +57,10 @@ pub mod pallet {
 		offchain::{AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer},
 		pallet_prelude::*,
 	};
-	use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
+	use minicbor::encode::Encoder;
+	use parity_scale_codec::alloc::vec::Vec as AllocVec;
 	use scale_info::{prelude::fmt, TypeInfo};
+	use sp_core::hashing::blake2_256;
 	use sp_runtime::{offchain::http, sp_std::str};
 
 	#[pallet::pallet]
@@ -58,7 +68,7 @@ pub mod pallet {
 
 	#[pallet::config]
 	pub trait Config:
-		frame_system::Config + CreateSignedTransaction<Call<Self>> + fmt::Debug
+		frame_system::Config + timestamp::Config + CreateSignedTransaction<Call<Self>> + fmt::Debug
 	{
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
@@ -91,7 +101,19 @@ pub mod pallet {
 	/// The first value is the median price
 	/// The second value is the age of the median price
 	#[pallet::storage]
-	pub type Price<T> = StorageValue<_, (u32, u16)>;
+	pub type Price<T> = StorageValue<_, (OracleMessage, u16)>;
+
+	/// Signatures are indexed by oracle message timestamp.
+	/// Second key is the signatory pub key, value is the signature bytes.
+	#[pallet::storage]
+	pub type SignatureStorage<T: Config> = StorageDoubleMap<
+		Hasher1 = Twox64Concat,
+		Key1 = u64,
+		Hasher2 = Identity,
+		Key2 = T::AccountId,
+		Value = [u8; 64],
+		QueryKind = OptionQuery,
+	>;
 
 	/// oracle genesis config definition and associated macros
 	// see https://docs.substrate.io/reference/how-to-guides/basics/configure-genesis-state/
@@ -135,7 +157,7 @@ pub mod pallet {
 			non_outlier_prices: Vec<u32>,
 			outliers: u16,
 			outlier_prices: Vec<u32>,
-			reward_elegible_nodes: Vec<T::AccountId>
+			rewards: Vec<T::AccountId>,
 		},
 		AggregationNotPerformed,
 	}
@@ -157,6 +179,12 @@ pub mod pallet {
 			who: T::AccountId,
 			when: BlockNumberFor<T>,
 		},
+		StoredSignature {
+			message: OracleMessage,
+			who: T::AccountId,
+			when: BlockNumberFor<T>,
+			signature: T::Signature,
+		},
 		Status {
 			median_price: u32,
 			flag: Flag,
@@ -165,6 +193,59 @@ pub mod pallet {
 			block: BlockNumberFor<T>,
 			status: AggregationStatus<T>,
 		},
+	}
+
+	#[derive(Clone, Encode, Decode, Eq, PartialEq, Debug, MaxEncodedLen, TypeInfo)]
+	pub struct OracleMessage {
+		pub median_price: u32,
+		pub timestamp: u64,
+		pub rewards: BoundedVec<[u8; 32], ConstU32<64>>, // Vec of byte arrays for ed25519 public keys
+	}
+
+	impl Default for OracleMessage {
+		fn default() -> Self {
+			OracleMessage { median_price: 0, timestamp: 0, rewards: BoundedVec::default() }
+		}
+	}
+
+	impl OracleMessage {
+		pub fn to_cardano_cbor(&self) -> AllocVec<u8> {
+			let mut buf = AllocVec::new();
+			let mut encoder = Encoder::new(&mut buf);
+
+			// Write tag 121
+			encoder.tag(minicbor::data::Tag::new(121)).unwrap();
+
+			// Start indefinite-length array
+			encoder.begin_array().unwrap();
+
+			// Add median_price
+			encoder.u32(self.median_price).unwrap();
+
+			// Add timestamp (as u32 if it fits, otherwise as u64)
+			if self.timestamp <= u32::MAX as u64 {
+				encoder.u32(self.timestamp as u32).unwrap();
+			} else {
+				encoder.u64(self.timestamp).unwrap();
+			}
+
+			// Add rewards as array of byte strings
+			encoder.begin_array().unwrap();
+			for reward_account in &self.rewards {
+				encoder.bytes(reward_account).unwrap();
+			}
+			encoder.end().unwrap(); // End rewards array
+
+			// End main array
+			encoder.end().unwrap();
+
+			buf
+		}
+
+		pub fn cardano_cbor_hash(&self) -> [u8; 32] {
+			let cbor_data = self.to_cardano_cbor();
+			blake2_256(&cbor_data)
+		}
 	}
 
 	/// pallet calls
@@ -177,6 +258,27 @@ pub mod pallet {
 			let when = <frame_system::Pallet<T>>::block_number();
 			NodesPrices::<T>::insert(&who, (price, when));
 			Self::deposit_event(Event::StoredPrice { price, who: who.clone(), when });
+			Ok(())
+		}
+
+		#[pallet::call_index(1)]
+		#[pallet::weight((0, Pays::No))]
+		pub fn store_signature(
+			origin: OriginFor<T>,
+			message: OracleMessage,
+			signature: T::Signature,
+		) -> DispatchResult {
+			let who: T::AccountId = ensure_signed(origin)?;
+			let when = <frame_system::Pallet<T>>::block_number();
+
+			let mut signature_bytes: AllocVec<u8> = signature.encode();
+			signature_bytes.remove(0);
+			let signature_encoded: [u8; 64] =
+				signature_bytes.try_into().expect("signature buffer should be exactly 64 bytes");
+			SignatureStorage::<T>::insert(message.timestamp, &who, signature_encoded);
+
+			Self::deposit_event(Event::StoredSignature { message, who, when, signature });
+
 			Ok(())
 		}
 	}
@@ -200,37 +302,52 @@ pub mod pallet {
 				Some(signer_account) if acc_list.next().is_none() => {
 					let signer = Signer::<T, T::AuthorityId>::all_accounts()
 						.with_filter(vec![signer_account.clone().public]);
+
 					if signer.can_sign() {
-						match Self::fetch_price() {
-							Ok(price) => {
-								let result = signer.send_single_signed_transaction(
-									&signer_account,
-									Call::store_price { price },
-								);
-								if result.is_some_and(|res| res.is_ok()) {
-									log::info!(
-										"[{:?}]: submit transaction success.",
-										signer_account.id
-									)
-								} else {
-									log::error!(
-										"[{:?}]: submit transaction failure.",
-										signer_account.id
-									)
-								}
-							},
-							Err(e) => {
+						if let Ok(price) = Self::fetch_price().map_err(|e| {
+							log::error!(
+								"[{:?}]: failed to fetch price: {:?}",
+								signer_account.id,
+								e
+							);
+						}) {
+							let result = signer.send_single_signed_transaction(
+								&signer_account,
+								Call::store_price { price },
+							);
+							if result.is_some_and(|res| res.is_ok()) {
+								log::info!(
+									"[{:?}]: submit store price transaction success.",
+									signer_account.id
+								)
+							} else {
 								log::error!(
-									"[{:?}]: failed to fetch price: {:?}",
-									signer_account.id,
-									e
-								);
-							},
+									"[{:?}]: submit store price transaction failure.",
+									signer_account.id
+								)
+							}
+						}
+						if let Some((message, signature)) = Self::sign_oracle_message(&signer) {
+							let result = signer.send_single_signed_transaction(
+								&signer_account,
+								Call::store_signature { message, signature },
+							);
+							if result.is_some_and(|res| res.is_ok()) {
+								log::info!(
+									"[{:?}]: submit store signature transaction success.",
+									signer_account.id
+								)
+							} else {
+								log::error!(
+									"[{:?}]: submit store signature transaction failure.",
+									signer_account.id
+								)
+							}
 						}
 					}
 				},
 				Some(_accounts) => log::error!("More than one account. Expected only one"),
-				None => log::error!("No account available for oracle"),
+				_none => log::error!("No account available for oracle"),
 			}
 		}
 
@@ -246,7 +363,7 @@ pub mod pallet {
 				let mut participating_nodes: u32 = 0;
 				let prices = NodesPrices::<T>::iter()
 					.by_ref()
-					.filter_map(|(k, (p, a))|  {
+					.filter_map(|(k, (p, a))| {
 						if (n - a) <= feed_age.into() {
 							participating_nodes += 1;
 							Some((k, p))
@@ -255,26 +372,30 @@ pub mod pallet {
 						}
 					})
 					.collect();
-				let (median_price, age, flag, status): (u32, u16, Flag, crate::AggregationStatus<T>) =
-					if min_nodes_for_trusted_aggregation <= participating_nodes {
-						log::info!(
-							"{:?} nodes submitted a price. Aggregating median price ...",
-							participating_nodes
-						);
-						Self::aggregate(prices, outliers_range, divergence_percentage)
-					} else {
-						log::error!("Not enough nodes for trusted aggregation. Reusing median ...");
-						Self::reuse_previous_median()
-					};
-				Price::<T>::put((median_price, age));
+				let (oracle_message, age, flag, status): (
+					OracleMessage,
+					u16,
+					Flag,
+					crate::AggregationStatus<T>,
+				) = if min_nodes_for_trusted_aggregation <= participating_nodes {
+					log::info!(
+						"{:?} nodes submitted a price. Aggregating median price ...",
+						participating_nodes
+					);
+					Self::aggregate(prices, outliers_range, divergence_percentage)
+				} else {
+					log::error!("Not enough nodes for trusted aggregation. Reusing median ...");
+					Self::reuse_previous_median()
+				};
+				Price::<T>::put((&oracle_message, age));
 				log::info!(
 					"Median price for block {:?} is {:?} with status: {:?}",
 					n,
-					median_price,
+					oracle_message.median_price,
 					flag
 				);
 				Self::deposit_event(Event::Status {
-					median_price,
+					median_price: oracle_message.median_price,
 					flag,
 					participating_nodes,
 					age,
@@ -293,33 +414,55 @@ impl<T: Config> Pallet<T> {
 		acc_and_prices: Vec<(T::AccountId, u32)>,
 		outliers_range: u32,
 		divergence_percentage: u32,
-	) -> (u32, u16, Flag, crate::AggregationStatus<T>) {
-		let mut acc_and_prices = BoundedVec::<(T::AccountId, u32), ConstU32<32>>::truncate_from(acc_and_prices);
+	) -> (OracleMessage, u16, Flag, crate::AggregationStatus<T>) {
+		let mut acc_and_prices =
+			BoundedVec::<(T::AccountId, u32), ConstU32<32>>::truncate_from(acc_and_prices);
 		acc_and_prices.sort_by_key(|k| k.1);
 		let sorted_acc_and_prices = acc_and_prices.to_vec();
 		let length: usize = acc_and_prices.len();
-		let (_addresses, sorted_prices): (Vec<T::AccountId>, Vec<u32>) = sorted_acc_and_prices.clone().into_iter().unzip();
-		let (median, (non_outlier_prices, outlier_prices)): (u32, (Vec<u32>, Vec<u32>)) =
-			if length > 1 {
-				let med = Self::calculate_median(sorted_prices.clone(), length);
-				let filtered = Self::filter_outliers(
-					sorted_prices,
-					med,
-					length,
-					outliers_range,
-					divergence_percentage,
-				);
-				(med, filtered)
-			} else {
-				(sorted_prices[0], (sorted_prices, vec![]))
-			};
-        let reward_elegible_nodes: Vec<T::AccountId> = sorted_acc_and_prices
-			.into_iter()
-			.filter(|(_, price)| non_outlier_prices.contains(price))
-			.map(|(account, _)| account)
-			.collect();
-		(
+		let (_addresses, sorted_prices): (Vec<T::AccountId>, Vec<u32>) =
+			sorted_acc_and_prices.clone().into_iter().unzip();
+		let median = Self::calculate_median(sorted_prices.clone(), length);
+		let (non_outlier_prices, outlier_prices) = Self::filter_outliers(
+			sorted_prices,
 			median,
+			length,
+			outliers_range,
+			divergence_percentage,
+		);
+		let rewards: Vec<T::AccountId> = sorted_acc_and_prices
+			.into_iter()
+			.filter_map(
+				|(account, price)| {
+					if non_outlier_prices.contains(&price) {
+						Some(account)
+					} else {
+						None
+					}
+				},
+			)
+			.collect();
+		// Get timestamp in milliseconds
+		let now_millis = timestamp::Pallet::<T>::get().saturated_into::<u64>();
+		let msg = OracleMessage {
+			median_price: median,
+			timestamp: now_millis,
+			rewards: BoundedVec::truncate_from(
+				rewards
+					.iter()
+					.map(|id| {
+						let account_bytes = id.encode();
+						let account_encoded: [u8; 32] = account_bytes
+							.try_into()
+							.expect("Account buffer should be exactly 32 bytes");
+						account_encoded
+					})
+					.collect(),
+			),
+		};
+
+		(
+			msg,
 			0,
 			Flag::Ok,
 			AggregationStatus::AggregationPerformed {
@@ -327,17 +470,22 @@ impl<T: Config> Pallet<T> {
 				non_outlier_prices,
 				outliers: outlier_prices.len() as u16,
 				outlier_prices,
-				reward_elegible_nodes
+				rewards,
 			},
 		)
 	}
 
-	fn reuse_previous_median() -> (u32, u16, Flag, crate::AggregationStatus<T>) {
+	fn reuse_previous_median() -> (OracleMessage, u16, Flag, crate::AggregationStatus<T>) {
 		if let Some((median, age)) = Price::<T>::get() {
 			(median, age + 1, Flag::NotEnoughNodes, AggregationStatus::AggregationNotPerformed)
 		} else {
 			log::error!("Error: no median to reuse.");
-			(0, 0, Flag::NoPreviousMedian, AggregationStatus::AggregationNotPerformed)
+			(
+				OracleMessage::default(),
+				0,
+				Flag::NoPreviousMedian,
+				AggregationStatus::AggregationNotPerformed,
+			)
 		}
 	}
 
@@ -413,5 +561,37 @@ impl<T: Config> Pallet<T> {
 		} else {
 			x - y
 		}
+	}
+
+	fn sign_oracle_message(
+		signer: &Signer<T, <T as Config>::AuthorityId, frame_system::offchain::ForAll>,
+	) -> Option<(OracleMessage, T::Signature)> {
+		let (message, age) = Price::<T>::get()?;
+
+		if age != 0 {
+			return None;
+		}
+
+		log::info!("Prepared Message: {:?}", message);
+		let cbor_hex: Box<str> = message.to_cardano_cbor().encode_hex();
+		log::debug!("Message cbor: {}", cbor_hex);
+
+		let msg_hash_digest = message.cardano_cbor_hash();
+		let msg_hash_hex: Box<str> = msg_hash_digest.encode_hex();
+		log::debug!("Message hash: {}", msg_hash_hex);
+
+		let signed_message = match signer.sign_message(&msg_hash_digest).pop() {
+			Some(signed) => signed,
+			_none => {
+				log::error!("Couldn't retrieve signature");
+				return None;
+			},
+		};
+
+		log::info!("Account signed: {:?}", signed_message.0.id);
+		let hex_signature: Box<str> = signed_message.1.encode().encode_hex();
+		log::info!("Signed message: {}", hex_signature);
+
+		Some((message, signed_message.1))
 	}
 }
