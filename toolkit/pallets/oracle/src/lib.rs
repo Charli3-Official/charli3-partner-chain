7cf33c5a0c18349c2c2ce5ddc56a22a4b40d93c4
xxAVOGADROxx <jose.seraquive@gmail.com>
2025-08-07 02:25:16 -0500

    WIP: package error

--- toolkit/pallets/oracle/src/lib.rs
+++ toolkit/pallets/oracle/src/lib.rs
@@ -63,9 +63,8 @@ pub mod pallet {
 
     #[pallet::config]
     pub trait Config:
-        frame_system::Config + timestamp::Config + CreateSignedTransaction<Call<Self>> + fmt::Debug
+        frame_system::Config<RuntimeEvent: From<Event<Self>>> + timestamp::Config + CreateSignedTransaction<Call<Self>> + fmt::Debug
     {
-        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
         type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
     }
 
