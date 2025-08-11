2f40e4ec8f34a2d1fc1fd913dae178d424a00a53
stanislav-az <st.zhdanovich@gmail.com>
2025-08-11 14:14:29 +0500

    fix runtime lib errors

--- toolkit/pallets/oracle/src/lib.rs
+++ toolkit/pallets/oracle/src/lib.rs
@@ -62,9 +62,10 @@ pub mod pallet {
     pub struct Pallet<T>(_);
 
     #[pallet::config]
-    pub trait Config:
-        frame_system::Config<RuntimeEvent: From<Event<Self>>> + timestamp::Config + CreateSignedTransaction<Call<Self>> + fmt::Debug
-    {
+    pub trait Config: frame_system::Config + SigningTypes + CreateSignedTransaction<Call<Self>> + pallet_timestamp::Config + fmt::Debug {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        /// AuthorityId for offchain signing. Uses the associated `Public`/`Signature` from SigningTypes.
         type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
     }
 
